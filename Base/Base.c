2fa6b8c09ef8bb1f9a15b09bf1a14a1ba7db361d
domokos <domokos@gmail.com>
2014-07-10 17:59:15 +0200

    Initial commit

---
+++ Base/Base.c
@@ -0,0 +1,145 @@
+/*
+ * Base.c
+ *
+ *  Created on: Oct 28, 2012
+ *      Author: doma
+ *      Basic common routines for the RS485 device
+ */
+#include "Base.h"
+
+// Global variables
+bool timer_initialized = FALSE;
+static volatile unsigned int  time_counter;
+
+static unsigned int timer_start_times[5];
+
+/*
+ * Internal utility functions
+ */
+
+
+// The timer ISR - set up to occur every 1 ms for a 11.0592 MHz Crystal
+ISR(TIMER0,0)
+{
+  // Increase timer
+  time_counter++;
+
+  // Keep watchdog happy
+  WATCHDOG_PIN = !WATCHDOG_PIN;
+
+  // Restart the timer
+  TR0  = 0;
+
+// use 17 machine cycles less to compensate for time
+// spent executing the ISR itself
+
+#ifdef  CRYSTAL_SPEED_LO
+  TL0  = 0x50;   // Restart from 0xfc66 (1 ms) - decimal 1c = 0xfc50
+  TH0  = 0xfc;
+#elif defined CRYSTAL_SPEED_HI
+  TL0  = 0xb6;   // Restart from 0xf8cc (1 ms) - decimal 1c = 0xf8b6
+  TH0  = 0xf8;
+#else
+#error "No or incorrect crystal speed defined."
+#endif
+  TR0  = 1;
+}
+
+
+/*
+ * Public functions
+ */
+
+// Initiaize the timer
+void init_timer(void)
+{
+  TR0  = 0;
+#ifdef  CRYSTAL_SPEED_LO
+  TL0  = 0x50;    // Start from 0xfc50
+  TH0  = 0xfc;
+#elif defined CRYSTAL_SPEED_HI
+  TL0  = 0xb6;    // Start from 0xf8b6
+  TH0  = 0xf8;
+#else
+#error "No or incorrect crystal speed defined."
+#endif
+  TMOD = (TMOD&0xF0)|0x01;    // Set Timer 0 16-bit mode
+  TR0  = 1;       // Start Timer 0
+  ET0  = 1;      // Enable Timer0 interrupt
+  timer_initialized = TRUE;
+}
+
+
+// Wait for seconds
+void delay_sec(unsigned int sec)
+{
+  if ( sec == 0 ) return;   // Return if delaytime is zero
+
+  reset_timeout(DELAY_TIMEOUT);
+
+  while ( !timeout_occured(DELAY_TIMEOUT, ONE_SEC_TIMEOUT))
+    {
+      __asm nop __endasm;
+    }             // Wait delaytime
+  return;
+}
+
+// Wait for miliseconds
+void delay_msec(unsigned int msec)
+{
+  if ( msec == 0 ) return;   // Return if delaytime is zero
+
+/*      Crystal: 11.0592 MHz
+ *      Freq. of 1 machine cycle (1 count) = 12 crystal pulse: 11059200/12 = 921600
+ *      Length of 1 machine cycle: 1/921600 sec
+ *      Clock ticks for 0.000100 = 100 usec (): 0.0001/(1/921600) = 92.16 ticks
+ *      Clock ticks for 0.001 = 1 ms : 921.6 ticks
+ *      We will use 922 ticks - to reload timer registers: FC66
+ */
+
+  reset_timeout(DELAY_TIMEOUT);
+
+  while ( !timeout_occured(DELAY_TIMEOUT, ONE_MS_TIMEOUT))
+    {
+      __asm nop __endasm;
+    }             // Wait delaytime
+  return;
+}
+
+
+// start and reset the messaging timeout counter
+void reset_timeout(unsigned char type)
+{
+  // Initialize timer if it is not initialized
+  if(!timer_initialized) init_timer();
+
+  timer_start_times[type] = time_counter;
+}
+
+// Get the time elapsed since reset
+unsigned int get_time_elapsed(unsigned char type)
+{
+  unsigned int counter;
+
+  // Get the time counter value
+  ET0  = 0;
+  counter = time_counter;
+  ET0  = 1;
+
+  // If there is no owerflow in the interrupt ticks
+  // (equality is regarded as no timeout - just started)
+  if (counter >= timer_start_times[type])
+    {
+      return counter - timer_start_times[type];
+  // There was an overflow - no multiple overflow is expected - timer must firs be reset and then queried regularily
+    } else {
+      return ((unsigned int)((unsigned int) 0xffff - timer_start_times[type])) + counter;
+    }
+}
+
+// Return if there was a timeout
+// The calling parameter holds the timeout limit in miliseconds
+bool timeout_occured(unsigned char type, unsigned int timeout_limit)
+{
+ return get_time_elapsed(type) >= timeout_limit;
+}
---
+++ Base/Base.h
@@ -0,0 +1,79 @@
+/*
+ * Base.h
+ *
+ *  Created on: Oct 28, 2012
+ *      Author: doma
+ */
+
+#ifndef BASE_H_
+#define BASE_H_
+
+#include <stddef.h>
+#include "at89c4051.h"
+
+// Global constants
+#define TRUE 1
+#define FALSE 0
+
+// Speed of the crystal
+//#define CRYSTAL_SPEED_LO // 11.0592MHz
+#define CRYSTAL_SPEED_HI // 22.1184MHz
+
+typedef __bit bool;
+
+#define ONE_MS_TIMEOUT 1
+#define ONE_SEC_TIMEOUT 1000
+
+// Timeout types
+#define MSG_TIMEOUT 0
+#define RESPONSE_TIMEOUT 1
+#define DELAY_TIMEOUT 2
+
+#ifdef __CDT_PARSER__
+  #define TEMP_CONV_TIMER 3
+  #define PWM1_TIMER 4
+  #define UNSEEN_COMM_TIMER 3
+#elif defined MASTER_DEVICE
+  #define UNSEEN_COMM_TIMER 3
+#elif defined SLAVE_DEVICE
+  #define TEMP_CONV_TIMER 3
+  #define PWM1_TIMER 4
+#else
+  #error "Unknown device type for setting timer constants"
+#endif
+
+// Watchdog pin
+#define WATCHDOG_PIN P3_5
+
+/*
+ * Internal utility functions
+ */
+
+// The timer ISR routine prototype to be included in the main file
+ISR(TIMER0,0);
+
+/*
+ * Public functions
+ */
+
+// Initiaize the timer
+void init_timer(void);
+
+
+// Wait for seconds
+void delay_sec(unsigned int sec);
+
+// Wait for milliseconds
+void delay_msec(unsigned int msec);
+
+// start and reset the messaging timeout counter
+void reset_timeout(unsigned char type);
+
+// Get the time elapsed since reset
+unsigned int get_time_elapsed(unsigned char type);
+
+// Return if there was a timeout
+// The calling parameter holds the timeout limit in miliseconds
+bool timeout_occured(unsigned char type, unsigned int timeout_limit);
+
+#endif /* BASE_H_ */
